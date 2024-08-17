918d958a8c254347a70b1c31c2f379166fe464e9
MID3EE-ARCH <169875752+MID3EE-ARCH@users.noreply.github.com>
2024-08-17 16:48:08 +0530

    done including generating random colors now only black until 10div is left

---
+++ script.js
@@ -0,0 +1,61 @@
+//-------------Variables-----------------------
+let container = document.querySelector(".container");
+let limit = 16;
+const gridSize = 960;
+
+//--------------Function to Generate Random Color of divs----------------
+function getRandomColorComponent() {
+  return Math.floor(Math.random() * 256);
+}
+
+//----------Creates 16*16 grid------------
+function setGrid() {
+  resetGrid();
+  const cellsize = gridSize / limit;
+  for (let index = 0; index < limit; index++) {
+    let rowcontainer = document.createElement("div");
+    rowcontainer.classList.add("row-container");
+    for (let j = 0; j < limit; j++) {
+      let rowdiv = document.createElement("div");
+      rowdiv.classList.add("row-div");
+      rowdiv.style.width = `${cellsize}px`;
+      rowdiv.style.height = `${cellsize}px`;
+      rowcontainer.appendChild(rowdiv);
+    }
+    container.appendChild(rowcontainer);
+  }
+  const cells = document.querySelectorAll(".row-div");
+  cells.forEach((cell) => {
+    cell.addEventListener("mouseenter", () => {
+      const red = getRandomColorComponent();
+      const green = getRandomColorComponent();
+      const blue = getRandomColorComponent();
+      cell.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
+    });
+  });
+}
+
+function resetGrid() {
+  while (container.firstChild) {
+    container.removeChild(container.firstChild);
+  }
+}
+setGrid();
+
+function setLimit() {
+  limit = parseInt(
+    prompt("Enter the size of grid . (should be less than 100)"),
+  );
+  if (limit > 100 || Number.isNaN(limit)) {
+    alert("Grid Size should be less than 100");
+    return;
+  }
+  setGrid();
+}
+
+function reset() {
+  limit = 16;
+  setGrid();
+}
+document.querySelector("#limit-button").addEventListener("click", setLimit);
+document.querySelector("#reset-button").addEventListener("click", reset);
