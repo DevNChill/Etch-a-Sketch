37ed8758ff4dd1faae2d98c4ff359e9df412ca75
DevNChill <169875752+DevNChill@users.noreply.github.com>
2024-09-09 19:47:46 +0530

    added opacity and random buttons

--- script.js
+++ script.js
@@ -3,6 +3,10 @@ let container = document.querySelector(".container");
 let limit = 16;
 const gridSize = 960;
 let globalOpacity = 0.2;
+let randomButton = document.querySelector(".rainbow");
+let opacityButton = document.querySelector(".opacitybutton");
+let randomColor = false;
+let opacityEnable = false;
 
 //--------------Function to Generate Random Color of divs----------------
 function getRandomColorComponent() {
@@ -28,13 +32,21 @@ function setGrid() {
   const cells = document.querySelectorAll(".row-div");
   cells.forEach((cell) => {
     cell.addEventListener("mouseenter", () => {
-      const red = getRandomColorComponent();
-      const green = getRandomColorComponent();
-      const blue = getRandomColorComponent();
-      cell.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
-      globalOpacity += 0.1;
-      if (globalOpacity >= 1) {
-        globalOpacity = 0.2;
+      if (randomColor) {
+        const red = getRandomColorComponent();
+        const green = getRandomColorComponent();
+        const blue = getRandomColorComponent();
+        cell.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
+      } else {
+        cell.style.backgroundColor = "grey";
+      }
+      if (opacityEnable) {
+        globalOpacity += 0.1;
+        if (globalOpacity >= 1) {
+          globalOpacity = 0.2;
+        }
+      } else {
+        globalOpacity = 1;
       }
       cell.style.opacity = globalOpacity;
     });
@@ -45,6 +57,8 @@ function resetGrid() {
   while (container.firstChild) {
     container.removeChild(container.firstChild);
   }
+  randomColor = false;
+  opacityEnable = false;
 }
 setGrid();
 
@@ -53,7 +67,7 @@ function setLimit() {
     prompt("Enter the size of grid . (should be less than 100)"),
   );
   if (limit > 100 || Number.isNaN(limit)) {
-    alert("Grid Size should be less than 100");
+    alert("Grid Size should be less than or equal to 100");
     return;
   }
   setGrid();
@@ -63,5 +77,17 @@ function reset() {
   limit = 16;
   setGrid();
 }
+randomButton.addEventListener("click", () => {
+  randomColor == false
+    ? ((randomColor = true), (randomButton.style.backgroundColor = "pink"))
+    : ((randomColor = false), (randomButton.style.backgroundColor = "#5783db"));
+});
+
+opacityButton.addEventListener("click", () => {
+  opacityEnable == true
+    ? ((opacityEnable = false),
+      (opacityButton.style.backgroundColor = "#5783db"))
+    : ((opacityEnable = true), (opacityButton.style.backgroundColor = "pink"));
+});
 document.querySelector("#limit-button").addEventListener("click", setLimit);
 document.querySelector("#reset-button").addEventListener("click", reset);
